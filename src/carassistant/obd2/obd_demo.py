ddd43ae5a3bf7d3e28d1ea975766c661f684de17
Peter Ott <ott.peter.r@gmail.com>
2020-05-02 17:58:09 -0400

    OBD Demo from repository

---
+++ src/carassistant/obd2/obd_demo.py
@@ -0,0 +1,14 @@
+import obd 
+connection = obd.OBD(timeout=1.5) # auto-connects to USB or RF port
+
+# obd.logger.setLevel(obd.logging.DEBUG)
+if connection is None:
+    #TODO: log that the OBD connection couldnt be made
+    exit()
+
+cmd = obd.commands.SPEED # select an OBD command (sensor)
+
+response = connection.query(cmd) # send the command, and parse the response
+
+print(response.value) # returns unit-bearing values thanks to Pint
+print(response.value.to("mph")) # user-friendly unit conversions
